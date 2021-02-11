51794f02b073499ec95485396cee0c9be3ed0732
Irfan <irfan@MacBook-Pro.local>
2021-02-11 18:11:01 -0500

    added toggle effects

--- src/App.js
+++ src/App.js
@@ -1,40 +1,47 @@
 import "./App.css";
 import * as Tone from "tone";
 import { useEffect, useState } from "react";
+import { keyToNote } from "./utils/keymaps";
 
 function App() {
   const synth = new Tone.PolySynth().toDestination();
 
   const [octave, setOctave] = useState(3);
 
-  //should be in utils
+  // const whitekeys = "asdfghjkl;'".split("");
+  // const blackkeys = "q  w-e-r-t  y-u-i  o-p-[".split("-");
 
-  const keyToNote = {
-    a: "F",
-    s: "G",
-    d: "A",
-    f: "B",
-    g: "C",
-    h: "D",
-    j: "E",
-    k: "F",
-    l: "G",
-    ";": "A",
-    "'": "B",
-    q: "F#",
-    w: "F#",
-    e: "G#",
-    r: "A#",
-    t: "C#",
-    y: "C#",
-    u: "D#",
-    i: "F#",
-    o: "F#",
-    p: "G#",
-    "[": "A#",
-  };
+  // toggled state when keypressed or clicked
 
-  function getNote(keyPressed) {
+  const [appState, changeState] = useState({
+    blackobjects: [
+      { id: "q  w", toggled: false },
+      { id: "e", toggled: false },
+      { id: "r", toggled: false },
+      { id: "t y", toggled: false },
+      { id: "u", toggled: false },
+      { id: "i  o", toggled: false },
+      { id: "p", toggled: false },
+      { id: "[", toggled: false },
+    ],
+    whiteobjects: [
+      { id: "a", toggled: false },
+      { id: "s", toggled: false },
+      { id: "d", toggled: false },
+      { id: "f", toggled: false },
+      { id: "g", toggled: false },
+      { id: "h", toggled: false },
+      { id: "j", toggled: false },
+      { id: "k", toggled: false },
+      { id: "l", toggled: false },
+      { id: ";", toggled: false },
+      { id: "'", toggled: false },
+    ],
+  });
+
+  // function for shifting octave in the keyboard
+
+  function getNote(key) {
     let lowerString = ["q", "a", "w", "s", "e", "d", "r", "f"];
     let upperString = [
       "g",
@@ -52,12 +59,12 @@ function App() {
       "'",
       "[",
     ];
-    debugger;
-    if (lowerString.indexOf(keyPressed) !== -1) {
-      return `${keyToNote[keyPressed]}${octave}`;
+
+    if (lowerString.indexOf(key) !== -1) {
+      return `${keyToNote[key]}${octave}`;
     }
-    if (upperString.indexOf(keyPressed) !== -1) {
-      return `${keyToNote[keyPressed]}${octave + 1}`;
+    if (upperString.indexOf(key) !== -1) {
+      return `${keyToNote[key]}${octave + 1}`;
     }
     return;
   }
@@ -83,14 +90,40 @@ function App() {
 
   function downHandler(event) {
     const { key } = event;
-
-    synth.triggerAttack(getNote(key));
+    let lowkey = key.toLowerCase();
+    synth.triggerAttack(getNote(lowkey));
+    if ("asdfghjkl;'".includes(lowkey)) {
+      let whiteindex = appState.whiteobjects.findIndex((i) =>
+        i.id.includes(lowkey)
+      );
+      toggleActiveWhite(whiteindex);
+    } else if ("qwertyuiop[".includes(lowkey)) {
+      let blackindex = appState.blackobjects.findIndex((i) =>
+        i.id.includes(lowkey)
+      );
+      toggleActiveBlack(blackindex);
+    } else {
+      return;
+    }
   }
 
   function upHandler(event) {
     const { key } = event;
-
-    synth.triggerRelease(getNote(key));
+    let lowkey = key.toLowerCase();
+    synth.triggerRelease(getNote(lowkey));
+    if ("asdfghjkl;'".includes(key)) {
+      let whiteindex = appState.whiteobjects.findIndex((i) =>
+        i.id.includes(lowkey)
+      );
+      toggleActiveWhite(whiteindex);
+    } else if ("qwertyuiop[".includes(lowkey)) {
+      let blackindex = appState.blackobjects.findIndex((i) =>
+        i.id.includes(lowkey)
+      );
+      toggleActiveBlack(blackindex);
+    } else {
+      return;
+    }
   }
 
   function playNote(note) {
@@ -101,38 +134,96 @@ function App() {
     synth.triggerRelease(`${note}`);
   }
 
-  const whitekeys = "asdfghjkl;'".split("");
-  const blackkeys = "q  w-e-r-t  y-u-i  o-p-[".split("-");
+  function toggleActiveBlack(index) {
+    let arrayCopy = [...appState.blackobjects];
+    arrayCopy[index].toggled
+      ? (arrayCopy[index].toggled = false)
+      : (arrayCopy[index].toggled = true);
+
+    changeState({ ...appState, blackobjects: arrayCopy });
+  }
+
+  function toggleActiveWhite(index) {
+    let arrayCopy = [...appState.whiteobjects];
+    arrayCopy[index].toggled
+      ? (arrayCopy[index].toggled = false)
+      : (arrayCopy[index].toggled = true);
+
+    changeState({ ...appState, whiteobjects: arrayCopy });
+  }
+
+  function toggleActiveStyleBlack(index) {
+    if (
+      appState.blackobjects[index].toggled ||
+      appState.whiteobjects[index].toggled
+    ) {
+      return "active";
+    } else {
+      return "inactive";
+    }
+  }
+
+  function toggleActiveStyleWhite(index) {
+    if (appState.whiteobjects[index].toggled) {
+      return "active";
+    } else {
+      return "inactive";
+    }
+  }
 
   return (
     <div className="App">
-      <div className="note-wrapper">
-        {blackkeys.map((blackkey) => (
-          <button
-            className="note-black"
-            onMouseDown={() => playNote(getNote(blackkey[0]))}
-            onMouseUp={() => stopNote(getNote(blackkey[0]))}
-          >
-            {blackkey.toUpperCase()}
-          </button>
-        ))}
-      </div>
+      <synth>
+        {/* black keys */}
 
-      {/* white keys */}
+        <div className="note-wrapper">
+          {appState.blackobjects.map((blackkey, index) => (
+            <button
+              className={
+                "note-black" +
+                " " +
+                blackkey.id +
+                " " +
+                toggleActiveStyleBlack(index)
+              }
+              key={index}
+              onMouseDown={() => {
+                playNote(getNote(blackkey.id[0]));
+              }}
+              onMouseUp={() => {
+                stopNote(getNote(blackkey.id[0]));
+                toggleActiveBlack(index);
+              }}
+            >
+              {blackkey.id.toUpperCase()}
+            </button>
+          ))}
+        </div>
 
-      <div className="note-wrapper">
-        {whitekeys.map((whitekey) => (
-          <button
-            className="note-white"
-            onMouseDown={(e) => {
-              playNote(getNote(whitekey));
-            }}
-            onMouseUp={() => stopNote(getNote(whitekey))}
-          >
-            {whitekey.toUpperCase()}
-          </button>
-        ))}
-      </div>
+        {/* white keys */}
+        <div className="note-wrapper">
+          {appState.whiteobjects.map((whitekey, index) => (
+            <button
+              className={
+                "note-white" +
+                " " +
+                whitekey.id +
+                " " +
+                toggleActiveStyleWhite(index)
+              }
+              onMouseDown={() => {
+                playNote(getNote(whitekey.id[0]));
+              }}
+              onMouseUp={() => {
+                stopNote(getNote(whitekey.id[0]));
+                toggleActiveWhite(index);
+              }}
+            >
+              {whitekey.id.toUpperCase()}
+            </button>
+          ))}
+        </div>
+      </synth>
     </div>
   );
 }
