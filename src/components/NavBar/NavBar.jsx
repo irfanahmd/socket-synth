9c356bf5d4a8656af8b696d09a388844e6fbebed
Irfan <irfan@MacBook-Pro.local>
2021-02-13 18:22:48 -0500

    refactored the NavBar's display based on auth status

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,12 +5,24 @@ import './NavBar.css';
 
 //look into NavLink on react-puppies-crud app for styling
 
-const NavBar = () => {
+const NavBar = (props) => {
+  let nav = props.user ? 
+
+  <div>
+    <Link to='' className='NavBar-link'>LOG OUT</Link>
+    &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+    <span className='NavBar-welcome'>WELCOME, {props.user.username}</span>
+  </div>
+    :
+  <div>
+    <Link to='/login' className='NavBar-link'>LOG IN</Link>
+    &nbsp;&nbsp;|&nbsp;&nbsp;
+    <Link to='/signup' className='NavBar-link'>SIGN UP</Link>
+  </div>;
+
   return (
     <div className='NavBar'>
-      <Link to="/login" className='NavBar-link'>Log In</Link>
-      &nbsp;&nbsp;&nbsp;&nbsp;
-      <Link to="/signup" className='NavBar-link'>Sign Up</Link>
+      {nav}
     </div>
   );
 };
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="App">
-      <NavBar />
+      <NavBar user={user} />
       <Switch>
         <Route
           exact
